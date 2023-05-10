6f3c4969d13ded6f73e7e7ae9ee93083f8e4286e
Melissa <melissadellorco1993@gmail.com>
2023-05-10 12:51:31 +0200

    Adapted TelemetryDto

--- src/main/java/it/pagopa/interop/probing/caller/dto/TelemetryDto.java
+++ src/main/java/it/pagopa/interop/probing/caller/dto/TelemetryDto.java
@@ -1,7 +1,10 @@
 package it.pagopa.interop.probing.caller.dto;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import it.pagopa.interop.probing.caller.util.EserviceStatus;
 import lombok.Builder;
@@ -27,10 +30,12 @@ public class TelemetryDto {
   private Long responseTime;
 
   @JsonProperty("koReason")
+  @Size(min = 1, max = 2048, message = "must be at least 1 and less than or equal to 2048")
   private String koReason;
 
   @JsonProperty("checkTime")
-  @NotNull(message = "must not be null")
+  @NotBlank(message = "must not be blank")
+  @Pattern(regexp = "\\d+", message = "should be numeric")
   private String checkTime;
 
 }
