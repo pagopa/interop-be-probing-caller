27d51d4af2ca31d5da165647b0382c4ea011a1c0
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-05-11 15:15:52 +0200

    added test properties file

--- src/main/java/it/pagopa/interop/probing/caller/InteropCallerApplication.java
+++ src/main/java/it/pagopa/interop/probing/caller/InteropCallerApplication.java
@@ -3,10 +3,8 @@ package it.pagopa.interop.probing.caller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableFeignClients
-@EnableScheduling
 @SpringBootApplication
 public class InteropCallerApplication {
 
