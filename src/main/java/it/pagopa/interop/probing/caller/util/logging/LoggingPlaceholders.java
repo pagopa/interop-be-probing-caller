5d149d27b7ac687d3cc5df1ac6629259d26fac9c
acolucci <a.colucci@dxc.com>
2023-07-11 11:40:49 +0200

    Removed cid with random value in logs

--- src/main/java/it/pagopa/interop/probing/caller/util/logging/LoggingPlaceholders.java
+++ src/main/java/it/pagopa/interop/probing/caller/util/logging/LoggingPlaceholders.java
@@ -3,7 +3,6 @@ package it.pagopa.interop.probing.caller.util.logging;
 public class LoggingPlaceholders {
   private LoggingPlaceholders() {}
 
-  public static final String TRACE_ID_PLACEHOLDER = "trace_id";
   public static final String TRACE_ID_XRAY_PLACEHOLDER = "AWS-XRAY-TRACE-ID";
   public static final String TRACE_ID_XRAY_MDC_PREFIX = "- [TRACE_ID= ";
 }
